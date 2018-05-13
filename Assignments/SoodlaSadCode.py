343377063cbdaa20ff8324d4317d5800b2d29b42
GoldenShiber <johan_soodla@hotmail.com>
2018-05-13 21:38:21 +0200

    happier code

--- Assignments/SoodlaSadCode.py
+++ Assignments/SoodlaSadCode.py
@@ -21,25 +21,27 @@ textFile2 = sc.textFile("/home/goldenshiber/LDSA/Spark/sv-en/europarl-v7.sv-en.s
 
 wordsData = textFile.map(lambda x: x.split(' '))
 
-print(wordsData.count())
+#print(wordsData.count())
 
 #print(wordsData.take(4))
 
 wordsData2 = textFile2.map(lambda x: x.split(' '))
+print(wordsData.take(4))
 
-print(wordsData2.count())
+#print(wordsData2.count())
 #print(wordsData2.take(4))
 
 def Func(lines):
     #lines = lines.lower()
-    lines = [[word.lower() for word in text.split()] for text in lines]
+    #lines = [[word.lower() for word in text.split()] for text in lines]
+    lines = [[word.lower() for word in lines] ]
     #lines = lines.split()
     return lines
 
 rdd1 = wordsData.map(Func)
 rdd2 = wordsData2.map(Func)
 
-#print(rdd1.take(4))
+print(rdd1.take(4))
 #print(rdd2.take(4))
 
 #remove the < cases
@@ -47,13 +49,13 @@ rdd2 = wordsData2.map(Func)
 filteredRDD = rdd1.filter(lambda x: not x[0].startswith('<'))
 filteredRDD2 = rdd2.filter(lambda x: not x[0].startswith('<'))
 
-print(filteredRDD.count())
-print(filteredRDD2.count())
+#print(filteredRDD.count())
+#print(filteredRDD2.count())
 
 
-#Record the line numbers associated with each line (hint: ZipWithIndex())
-#   rdd1.zipWithIndex()
-#rdd2.zipWithIndex()
+#Record the line numbers associated with each line (hindft: ZipWithIndex())
+rdd1=rdd1.zipWithIndex()
+rdd2=rdd2.zipWithIndex()
 
 
 
@@ -63,6 +65,8 @@ rdd2inverted = rdd2.map(lambda x:(x[1],x[0]))
 
 #Join the RDDs
 rdd_join=rdd1inverted.join(rdd2inverted)
-print(rdd_join.take(5))
-print(rdd_join.first())
+print(rdd_join.take(1))
+#print(rdd_join.first())
+
+
 
