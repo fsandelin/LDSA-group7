7e3be65dec9734234e0856421f3540b7eac70254
GoldenShiber <johan_soodla@hotmail.com>
2018-05-13 20:45:16 +0200

    FeelsBadMan code

---
+++ Assignments/SoodlaSadCode.py
@@ -0,0 +1,68 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer
+import pyspark.sql.functions as f
+
+import os
+
+from collections import Counter
+
+os.environ['PYSPARK_PYTHON'] = '/usr/bin/python3'
+
+spark = SparkSession.builder \
+ .appName("Python Spark SQL basic example") \
+ .config("spark.some.config.option", "some-value") \
+ .getOrCreate()
+
+sc = spark.sparkContext
+textFile = sc.textFile("/home/goldenshiber/LDSA/Spark/sv-en/europarl-v7.sv-en.en")
+
+textFile2 = sc.textFile("/home/goldenshiber/LDSA/Spark/sv-en/europarl-v7.sv-en.sv")
+
+wordsData = textFile.map(lambda x: x.split(' '))
+
+print(wordsData.count())
+
+#print(wordsData.take(4))
+
+wordsData2 = textFile2.map(lambda x: x.split(' '))
+
+print(wordsData2.count())
+#print(wordsData2.take(4))
+
+def Func(lines):
+    #lines = lines.lower()
+    lines = [[word.lower() for word in text.split()] for text in lines]
+    #lines = lines.split()
+    return lines
+
+rdd1 = wordsData.map(Func)
+rdd2 = wordsData2.map(Func)
+
+#print(rdd1.take(4))
+#print(rdd2.take(4))
+
+#remove the < cases
+
+filteredRDD = rdd1.filter(lambda x: not x[0].startswith('<'))
+filteredRDD2 = rdd2.filter(lambda x: not x[0].startswith('<'))
+
+print(filteredRDD.count())
+print(filteredRDD2.count())
+
+
+#Record the line numbers associated with each line (hint: ZipWithIndex())
+#   rdd1.zipWithIndex()
+#rdd2.zipWithIndex()
+
+
+
+#Swap the key and value - so that the line number is the key
+rdd1inverted = rdd1.map(lambda x:(x[1],x[0]))
+rdd2inverted = rdd2.map(lambda x:(x[1],x[0]))
+
+#Join the RDDs
+rdd_join=rdd1inverted.join(rdd2inverted)
+print(rdd_join.take(5))
+print(rdd_join.first())
+
